febb2be659597ba188cc1248313a652d255d658c
Gustavo Domingues <stremps.1@gmail.com>
2025-11-09 15:22:50 -0300

    Adicionando formulário

---
+++ assets/js/form.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // --- CONTROLE DE PERFIL DE INTERESSE ---
+    const radiosVinculo = document.querySelectorAll('input[name="vinculo"]');
+    const secaoUnioeste = document.getElementById('secao-unioeste');
+    const secaoExterno = document.getElementById('secao-externo');
+    const secaoPesquisador = document.getElementById('secao-pesquisador');
+    const avisoVinculo = document.getElementById('aviso-vinculo');
+
+    function esconderTodasSecoes() {
+        secaoUnioeste.classList.add('d-none');
+        secaoExterno.classList.add('d-none');
+        secaoPesquisador.classList.add('d-none');
+        avisoVinculo.classList.add('d-none');
+    }
+
+    radiosVinculo.forEach(radio => {
+        radio.addEventListener('change', (evento) => {
+            esconderTodasSecoes();
+            if (evento.target.value === 'unioeste') {
+                secaoUnioeste.classList.remove('d-none');
+            } else if (evento.target.value === 'externo') {
+                secaoExterno.classList.remove('d-none');
+            } else if (evento.target.value === 'pesquisador') {
+                secaoPesquisador.classList.remove('d-none');
+            }
+        });
+    });
+
+    // --- CONTROLE DA MATRIZ DE HABILIDADES ---
+    const checkboxesArea = document.querySelectorAll('.area-interesse-check');
+
+    checkboxesArea.forEach(checkbox => {
+        checkbox.addEventListener('change', (evento) => {
+            const linha = checkbox.closest('tr');
+            const rangeInput = linha.querySelector('.area-nivel-range');
+
+            if (checkbox.checked) { // Verifica se pode ativar o range
+                rangeInput.disabled = false;
+            } else {
+                rangeInput.disabled = true; 
+                rangeInput.value = 2; // Coloca o valor do range no meio
+            }
+        });
+    });
+
+});
