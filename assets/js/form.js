cc842fcdc4a173a27ba46d237cfb54bcf8d46188
Gustavo Domingues <stremps.1@gmail.com>
2025-11-09 20:46:34 -0300

    Adicionando campos obrigatórios dinâmicos, função onsubmit e pequenos comentários

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,17 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    // --- CONTROLE DE PERFIL DE INTERESSE ---
+    // --- CONTROLE DOS GRUPOS DE PERFIL DE INTERESSE ---
     const radiosVinculo = document.querySelectorAll('input[name="vinculo"]');
     const secaoUnioeste = document.getElementById('secao-unioeste');
     const secaoExterno = document.getElementById('secao-externo');
     const secaoPesquisador = document.getElementById('secao-pesquisador');
     const avisoVinculo = document.getElementById('aviso-vinculo');
 
+
+    function gerenciarObrigatoriedade(secao, valorObrigatorio) {
+        const listaCamposDinamicos = secao.querySelectorAll('.obrigatorio-dinamico'); // Busca na seção, os que tem a classe
+        
+        // Para cada campo encontrado define o valor de obrigatoriedade
+        listaCamposDinamicos.forEach(campo => {
+            campo.required = valorObrigatorio;
+        });
+    }
+
     function esconderTodasSecoes() {
         secaoUnioeste.classList.add('d-none');
         secaoExterno.classList.add('d-none');
         secaoPesquisador.classList.add('d-none');
         avisoVinculo.classList.add('d-none');
+
+        gerenciarObrigatoriedade(secaoUnioeste, false);
+        gerenciarObrigatoriedade(secaoExterno, false);
+        gerenciarObrigatoriedade(secaoPesquisador, false);
     }
 
     radiosVinculo.forEach(radio => {
@@ -19,10 +33,13 @@ document.addEventListener("DOMContentLoaded", () => {
             esconderTodasSecoes();
             if (evento.target.value === 'unioeste') {
                 secaoUnioeste.classList.remove('d-none');
+                gerenciarObrigatoriedade(secaoUnioeste, true);
             } else if (evento.target.value === 'externo') {
                 secaoExterno.classList.remove('d-none');
+                gerenciarObrigatoriedade(secaoExterno, true);
             } else if (evento.target.value === 'pesquisador') {
                 secaoPesquisador.classList.remove('d-none');
+                gerenciarObrigatoriedade(secaoPesquisador, true);
             }
         });
     });
@@ -45,3 +62,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+function avisoSubmissao(){
+    alert("Obrigado!");
+}
